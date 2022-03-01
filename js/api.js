99777d9130c430b2ee9f1131cb4fa3f2f6ad82ac
Abdul <abdulmomin956@gmail.com>
2022-03-01 23:07:47 +0600

    detail completed

--- js/api.js
+++ js/api.js
@@ -43,5 +43,49 @@ const detailBtn = data => {
     url = `https://openapi.programming-hero.com/api/phone/${data}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayDetail(data.data))
 }
+
+const displayDetail = data => {
+    const detailBox = document.getElementById('details');
+    detailBox.innerHTML = `
+    <img src="${data.image}">
+    <p>${data.releaseDate}</p>
+    <table id="table"></table>
+    `;
+    const features = data.mainFeatures;
+    const pairs = Object.entries(features);
+    const tableParent = document.getElementById('table');
+    tableParent.textContent = '';
+    for (const item of pairs) {
+        const tr = document.createElement('tr');
+        // comma separated array 
+        if (typeof item[1] === 'object') {
+            const arrayItem = item[1].join(', ');
+            tr.innerHTML = `<td>${item[0]}: </td><td>${arrayItem}</td>`;
+            tableParent.appendChild(tr);
+        }
+        else {
+            tr.innerHTML = `<td>${item[0]}: </td><td>${item[1]}</td>`;
+            tableParent.appendChild(tr);
+        }
+        // console.log(item[0], item[1]);
+    }
+    if (typeof data.others === 'object') {
+        const tableParent2 = document.createElement('table');
+        tableParent2.innerHTML = `
+        <tr><th style="font-weight:700;">Others</th></tr>
+        `;
+        const pairs2 = Object.entries(data.others);
+        for (const item of pairs2) {
+            const tr = document.createElement('tr');
+            tr.innerHTML = `<td>${item[0]}: </td><td>${item[1]}</td>`;
+            tableParent2.appendChild(tr);
+        }
+        detailBox.appendChild(tableParent2);
+    }
+
+
+    console.log(data.others);
+}
+
