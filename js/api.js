a7ec8aea2eed493b8c650cc60d38dde823f54e17
Abdul <abdulmomin956@gmail.com>
2022-03-01 13:09:24 +0600

    card added

--- js/api.js
+++ js/api.js
@@ -6,8 +6,25 @@ const phoneLoad = () => {
         .then(data => displayPhone(data.data))
 }
 const displayPhone = data => {
+    const container = document.getElementById('main');
     for (const mobile of data) {
-        console.log(mobile)
+        const phoneCard = document.createElement('div');
+        phoneCard.classList.add('col');
+        phoneCard.innerHTML = `
+        
+            <div class="card h-100">
+                <img style="max-width:240px" src="${mobile.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${mobile.phone_name}</h5>
+                    <p class="card-text">${mobile.brand}</p>
+                </div>
+                <div class="card-footer">
+                    <button type="button" class="btn btn-secondary">Details</button>
+                </div>
+            </div>
+                    `;
+        container.appendChild(phoneCard);
+        // console.log(mobile)
     }
 
 }
